f31f0693a28ffdb6d663ee25648949dbf6c6bf61
Fatan <fatan.aminullah.j@gmail.com>
2020-12-16 23:38:13 +0700

    update project section

--- src/module/projects/projects-component.js
+++ src/module/projects/projects-component.js
@@ -1,37 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 const ProjectsComponent = (props) => {
-    if (props.data) {
-      var projects = props.data.projects.map((projects) => {
-        var projectImage = 'images/portfolio/' + projects.image;
-        return (
-          <div key={projects.title} className="columns portfolio-item">
-            <div className="item-wrap">
-              <a href={projects.url} title={projects.title}>
-                <img alt={projects.title} src={projectImage} />
-                <div className="overlay">
-                  <div className="portfolio-item-meta">
-                    <h5>{projects.title}</h5>
-                    <p>{projects.category}</p>
-                  </div>
-                </div>
-              </a>
-            </div>
-          </div>
-        )
-      })
-    }
-    return (
-      <section id="portfolio">
-        <div className="row">
-          <div className="twelve columns collapsed">
-            <h1>Check Out Some of My Works.</h1>
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-              {projects}
+  if (props.data) {
+    var projectsWeb = props.data.projects.map((item) => {
+      var projectImage = "images/portfolio/" + item.image;
+      // if (item.type === "web") {
+      return (
+        <div key={item.title} className="columns portfolio-item">
+          <div className="item-wrap">
+            <img alt={item.title} src={projectImage} />
+            <div className="overlay">
+              <div className="portfolio-item-meta">
+                <h5>{item.title}</h5>
+                <p>{item.category}</p>
+              </div>
             </div>
           </div>
         </div>
-      </section>
-    );
+      );
+      // }
+    });
+    // var projectsMobile = props.data.projects.map((item) => {
+    //   var projectImage = "images/portfolio/" + item.image;
+    //   if (item.type === "mobile") {
+    //     return (
+    //       <div key={item.title} className="columns portfolio-item">
+    //         <div className="item-wrap" style={{ width: 100 }}>
+    //           <img alt={item.title} src={projectImage} style={{width: 100}} />
+    //           <div className="overlay">
+    //             <div className="portfolio-item-meta">
+    //               <h5>{item.title}</h5>
+    //               <p>{item.category}</p>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     );
+    //   }
+    // });
   }
+  return (
+    <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1>Check Out Some of My Works.</h1>
+          <div
+            id="portfolio-wrapper"
+            className="bgrid-quarters s-bgrid-thirds cf"
+          >
+            {projectsWeb}
+            {/* <div className="row">
+              {projectsWeb}
+            </div>
+            <div className="row">
+              {projectsMobile}
+            </div> */}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 export default ProjectsComponent;
